92cbadf64a2b8f3db6a54aecc8d60b78cf49c1ae
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-24 20:54:46 +0000

    cleanup setup.py
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@4 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- setup.py
+++ setup.py
@@ -1,13 +1,42 @@
 from distutils.core import setup, Extension
 import numpy
+import os
 
-setup(name = "grib_api",
+grib_api_dir = os.environ.get('GRIBAPI_DIR')
+jasper_dir = os.environ.get('JASPER_DIR')
+png_dir = os.environ.get('PNG_DIR')
+zlib_dir = os.environ.get('ZLIB_DIR')
+openjpeg_dir = os.environ.get('OPENJPEG_DIR')
+libdirs=[]
+incdirs=[numpy.get_include()]
+libraries=['grib_api']
+if grib_api_dir: 
+    incdirs.append(os.path.join(grib_api_dir,'include'))
+    libdirs.append(os.path.join(grib_api_dir,'lib'))
+if openjpeg_dir: 
+    incdirs.append(os.path.join(openjpeg_dir,'include'))
+    libdirs.append(os.path.join(openjpeg_dir,'lib'))
+    libraries.append("openjpeg")
+if jasper_dir: 
+    incdirs.append(os.path.join(jasper_dir,'include'))
+    libdirs.append(os.path.join(jasper_dir,'lib'))
+    libraries.append("jasper")
+if png_dir: 
+    incdirs.append(os.path.join(png_dir,'include'))
+    libdirs.append(os.path.join(png_dir,'lib'))
+    libraries.append("png")
+if zlib_dir: 
+    incdirs.append(os.path.join(zlib_dir,'include'))
+    libdirs.append(os.path.join(zlib_dir,'lib'))
+    libraries.append("z")
+
+setup(name = "pygrib",
       version = "1.0",
       ext_modules = [Extension(
         "pygrib",
         ["pygrib.c"],
-	include_dirs=[numpy.get_include(),'/Users/jwhitaker/include','/opt/local/include'],
-        library_dirs=['/opt/local/lib','/Users/jwhitaker/lib'],
-        libraries=["grib_api","jasper","openjpeg","png"]
+	include_dirs=incdirs,
+        library_dirs=libdirs,
+        libraries=libraries
       )])
 
