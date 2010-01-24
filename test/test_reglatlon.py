f75f31bb47a5779d150004c7f321586474522efd
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-24 21:46:36 +0000

    add test for reg lat/lon grid
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@6 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/test_reglatlon.py
@@ -0,0 +1,26 @@
+import pygrib
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+for g in pygrib.open('../sampledata/gfs.grb'):
+    if g['name'] == 'Volumetric soil moisture content':
+        data = g['values']
+        lats,lons = g.latlons()
+        break
+print data.min(), data.max()
+print lats[:,0]
+print lons[0,:]
+llcrnrlon = lons[0,0]
+llcrnrlat = lats[0,0]
+urcrnrlon = lons[-1,-1]
+urcrnrlat = lats[-1,-1]
+print llcrnrlon,llcrnrlat,urcrnrlon,urcrnrlat
+m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,
+            resolution='c',projection='cyl')
+#m.scatter(lons.flat,lats.flat,1,marker='o',color='k',zorder=10)
+m.drawcoastlines()
+x,y = m(lons,lats)
+m.contourf(x,y,data,15)
+#m.fillcontinents()
+plt.title(g['name']+' Global Lat/Lon Grid')
+plt.show()
