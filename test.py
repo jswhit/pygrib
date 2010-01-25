2149297883bb7cd6e67b5eb932e50f8889e04e40
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-25 20:23:46 +0000

    update
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@28 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- test.py
+++ test.py
@@ -6,24 +6,25 @@ print '-- all messages --'
 for grb in grbs:
     print grb
 # position iterator at beginning again.
-grbs.rewind()
+grbs.rewind() # grb.rewind() will have same effect.
 print '-- all messages (again)  --'
 for grb in grbs:
     print grb
 # get grib a specific grib message from the iterator.
 # iterator will be positioned at this message.
-grb = grbs.message(2)
+grb = grbs.message(2) # grb.message(2) will have same effect
 print '-- 2nd message --'
 print grb # 2nd message
-# get just the next grib message.
-grb.next()
+# position iterator at next grib message.
+grb.next() # grbs.next() has the same effect
 print '-- 3rd message --'
 print grb # 3rd message
 # now the iterator should be positioned at the last (4th) message.
 print '-- iterate from 4th message to the end--'
 for grb in grbs:
     print grb # only last message printed.
-grb.rewind()
+# rewind again
+grbs.rewind()
 print '-- Maximium temperature --'
 # iterate over all messages until
 # 'Maximum temperature' is found.
