0b039534afe483b05f4e269e32e699e75fc656c1
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-25 03:39:24 +0000

    add tigge test
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@9 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/test_tigge.py
@@ -0,0 +1,24 @@
+import numpy as np
+from mpl_toolkits.basemap import Basemap
+from numpy import ma
+import pygrib
+import matplotlib.pyplot as plt
+
+for grb in pygrib.open('../sampledata/tigge.grb'):
+    fld = 0.01*grb['values'] # convert to hPa
+    lats,lons = grb.latlons()
+    print grb['centre'], fld.shape, fld.min(), fld.max()
+    fig=plt.figure(figsize=(10,5))
+    fig.add_axes([0.1,0.1,0.8,0.8])
+    # setup robinson world map projection.
+    m = Basemap(projection='robin',lon_0=180)
+    x, y = m(lons,lats)
+    levels = np.arange(475,1101,25)
+    CS = m.contourf(x,y,fld,levels,cmap=plt.cm.jet)
+    plt.colorbar(drawedges=True, shrink=0.8) # draw colorbar
+    m.drawcoastlines()
+    m.drawparallels(np.arange(-80,81,20),labels=[1,0,0,0])
+    m.drawmeridians(np.arange(0,360,60),labels=[0,0,0,1])
+    m.drawmapboundary()
+    plt.title(grb['name']+': '+grb['centre'].upper(),fontsize=12)
+plt.show()
