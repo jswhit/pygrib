d3eb2f67db32670d98fea9298b7898b46f4d04fe
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-25 03:33:58 +0000

    test for NDFD domain
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@8 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/test_ndfd_conus.py
@@ -0,0 +1,36 @@
+import pygrib
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+g = pygrib.open('../sampledata/ds.maxt.grb')
+g.next()
+lats, lons = g.latlons()
+data = g['values']
+print data.dtype
+print data.shape, lons.shape, lats.shape
+print data.min(), data.max()
+llcrnrlon = lons[0,0]
+llcrnrlat = lats[0,0]
+urcrnrlon = lons[-1,-1]
+urcrnrlat = lats[-1,-1]
+rsphere = (g.projparams['a'],g.projparams['b'])
+lat_1 = g.projparams['lat_1']
+lat_2 = g.projparams['lat_2']
+lon_0 = g.projparams['lon_0']
+projection = g.projparams['proj']
+fig=plt.figure()
+print g.projparams
+ax = fig.add_axes([0.1,0.1,0.75,0.75])
+m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
+            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,rsphere=rsphere,lon_0=lon_0,
+            lat_1=lat_1,lat_2=lat_2,resolution='l',projection=projection,area_thresh=10000)
+x,y = m(lons, lats)
+cs = m.contourf(x,y,data,20,cmap=plt.cm.jet)
+m.drawcoastlines()
+m.drawstates()
+m.drawcountries()
+# new axis for colorbar.
+cax = plt.axes([0.875, 0.15, 0.03, 0.65])
+plt.colorbar(cs, cax, format='%g') # draw colorbar
+plt.axes(ax)  # make the original axes current again
+plt.title('NDFD Temp CONUS %d-h forecast'% g['forecastTime'],fontsize=12)
+plt.show()
