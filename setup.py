bacf39e6b3118fb6848ee308f8f8dff756631daf
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-01-27 02:10:17 +0000

    add lib64 to lib search path
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@48 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- setup.py
+++ setup.py
@@ -7,27 +7,34 @@ jasper_dir = os.environ.get('JASPER_DIR')
 png_dir = os.environ.get('PNG_DIR')
 zlib_dir = os.environ.get('ZLIB_DIR')
 openjpeg_dir = os.environ.get('OPENJPEG_DIR')
+
 libdirs=[]
 incdirs=[numpy.get_include()]
 libraries=['grib_api']
+
 if grib_api_dir: 
     incdirs.append(os.path.join(grib_api_dir,'include'))
     libdirs.append(os.path.join(grib_api_dir,'lib'))
+    libdirs.append(os.path.join(grib_api_dir,'lib64'))
 if openjpeg_dir: 
     incdirs.append(os.path.join(openjpeg_dir,'include'))
     libdirs.append(os.path.join(openjpeg_dir,'lib'))
+    libdirs.append(os.path.join(openjpeg_dir,'lib64'))
     libraries.append("openjpeg")
 if jasper_dir: 
     incdirs.append(os.path.join(jasper_dir,'include'))
     libdirs.append(os.path.join(jasper_dir,'lib'))
+    libdirs.append(os.path.join(jasper_dir,'lib64'))
     libraries.append("jasper")
 if png_dir: 
     incdirs.append(os.path.join(png_dir,'include'))
     libdirs.append(os.path.join(png_dir,'lib'))
+    libdirs.append(os.path.join(png_dir,'lib64'))
     libraries.append("png")
 if zlib_dir: 
     incdirs.append(os.path.join(zlib_dir,'include'))
     libdirs.append(os.path.join(zlib_dir,'lib'))
+    libdirs.append(os.path.join(zlib_dir,'lib64'))
     libraries.append("z")
 
 setup(name = "pygrib",
