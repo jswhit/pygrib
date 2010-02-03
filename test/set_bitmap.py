685045961a366230568f935a297a18e449d0192f
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-02-03 19:39:35 +0000

    update
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@76 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- test/set_bitmap.py
+++ test/set_bitmap.py
@@ -1,7 +1,7 @@
 import pygrib
 
 print 'grib_api version =',pygrib.api_version()
-infile = '../sampledata/regular_latlon_surface.grib1'
+infile = '../sampledata/regular_latlon_surface.grib2'
 outfile = 'out.grib1'
 grbs = pygrib.open(infile)
 print 'number of messages in file =',grbs.messages
