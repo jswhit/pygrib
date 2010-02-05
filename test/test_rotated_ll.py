c09767431a928aa5aba36f566f69856a46527161
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-02-05 23:46:34 +0000

    rotated lat/lon test
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@109 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/test_rotated_ll.py
@@ -0,0 +1,16 @@
+import pygrib
+import numpy as np
+import matplotlib.pyplot as plt
+import pyproj
+from mpl_toolkits.basemap import Basemap
+from PyNIO import nio
+grbs = pygrib.open('../sampledata/rotated_ll.grib1')
+grb = grbs.next()
+lats, lons = grb.latlons()
+data = grb['values']
+m = Basemap(projection='stere',lon_0=5,lat_0=60,width=4000.e3,height=3000.e3,resolution='l')
+x,y = m(lons,lats)
+m.drawcoastlines()
+m.contourf(x,y,data,15)
+plt.title(grb['name'])
+plt.show()
