dc4aaec0649b259535604e81e93f713d64896fa5
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-03-04 15:21:01 +0000

    remove obsolete file
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@146 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- testw.py
+++
@@ -1,9 +0,0 @@
-import pygrib
-import numpy as np
-grbs = pygrib.open('sampledata/flux.grb')
-for grb in grbs:
-    grb['centre']='ecmf'
-    grb['subCentre']=99
-    grb['values'] = np.ones((grb['Nj'],grb['Ni']),np.float)
-    grb.dump_message('flux.grb')
-grbs.close()
