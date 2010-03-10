b94a90f25e6fbdeb89a3cf8544acbf91a8f67899
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-03-10 22:21:36 +0000

    add simple animation example
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@155 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/animate.py
@@ -0,0 +1,42 @@
+import pygrib
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+from mpl_toolkits.basemap import Basemap
+
+# animation example.
+
+grbs = pygrib.open('../sampledata/safrica.grib2')
+# grab all "brightness temp" grib messages.
+btemps = [grb for grb in grbs if grb['name']=='Brightness temperature']
+lats, lons = grb.latlons()
+projd = grb.projparams
+grbs.close()
+print projd
+
+# create a map projection for the domain, plot 1st image on it.
+m =\
+Basemap(projection=projd['proj'],lat_ts=projd['lat_ts'],lon_0=projd['lon_0'],\
+        lat_0=projd['lat_0'],rsphere=(projd['a'],projd['b']),\
+        llcrnrlat=lats[0,0],urcrnrlat=lats[-1,-1],\
+        llcrnrlon=lons[0,0],urcrnrlon=lons[-1,-1],resolution='i')
+plt.figure(figsize=(8,7))
+plt.ion() # set interactive mode on
+m.drawcoastlines()
+m.drawcountries()
+grb = btemps[0]
+im = m.imshow(grb['values'],interpolation='nearest',vmin=230,vmax=310)
+plt.colorbar(orientation='horizontal')
+m.drawparallels(np.arange(-80,10,10),labels=[1,0,0,0])
+m.drawmeridians(np.arange(-80,81,20),labels=[0,0,0,1])
+plt.title(grb,fontsize=8)
+plt.draw()
+
+# loop 4 times, plot all images sequentially.
+for loop in range(4):
+   time.sleep(5)
+   for grb in btemps:
+       im.set_data(grb['values'])
+       plt.title(grb,fontsize=8)
+       plt.draw()
+time.sleep(5)
