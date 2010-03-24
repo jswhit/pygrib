0b38b6322978bc9909ac25a3d0342acd200d47c7
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-03-24 14:59:44 +0000

    update space_view test
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@184 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- test/test_eumetsat.py
+++ test/test_eumetsat.py
@@ -2,10 +2,12 @@ import pygrib
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+
 grbs = pygrib.open('../sampledata/eumetsat_precip.grb')
-#grb = grbs.next()
-grb = grbs.message(1)
+grb = grbs.next()
+print grb
 fld = grb['values']
+print fld.shape
 lats, lons = grb.latlons()
 rsphere = (grb.projparams['a'], grb.projparams['b'])
 lon_0 = grb.projparams['lon_0']
@@ -13,6 +15,7 @@ h = grb.projparams['h']
 print grb.projparams
 projection = grb.projparams['proj']
 
+plt.figure()
 m = Basemap(lon_0=lon_0,satellite_height=h,\
             rsphere = rsphere,\
             resolution='l',area_thresh=10000.,projection='geos')
@@ -21,15 +24,44 @@ x, y = m(lons,lats)
 m.scatter(x[::50,::50].flat,y[::50,::50].flat,1,marker='o',color='k',zorder=10)
 m.drawcoastlines()
 m.drawcountries()
-#m.fillcontinents(color='coral')
 m.drawcoastlines()
 # contour data.
-m.contourf(x,y,fld,20)
+m.contourf(x,y,fld,30)
 # pcolor image (slower)
 #m.pcolor(x,y,fld)
 m.drawparallels(np.arange(-80,81,20))
 m.drawmeridians(np.arange(-90,90,20))
 m.drawmapboundary()
-plt.title('EUMETSAT geostationary projection grid')
+plt.title('EUMETSAT geostationary projection grid 1')
+
+grb = grbs.next()
+print grb
+fld = grb['values']
+print fld.shape
+lats, lons = grb.latlons()
+rsphere = (grb.projparams['a'], grb.projparams['b'])
+lon_0 = grb.projparams['lon_0']
+h = grb.projparams['h']
+print grb.projparams
+grbs.close()
+
+plt.figure()
+m = Basemap(lon_0=lon_0,satellite_height=h,\
+            rsphere = rsphere,\
+            resolution='l',area_thresh=10000.,projection='geos')
+# plot every 30th point.
+x, y = m(lons,lats)
+m.scatter(x[::30,::30].flat,y[::30,::30].flat,1,marker='o',color='k',zorder=10)
+m.drawcoastlines()
+m.drawcountries()
+m.drawcoastlines()
+# contour data.
+m.contourf(x,y,fld,30)
+# pcolor image (slower)
+#m.pcolor(x,y,fld)
+m.drawparallels(np.arange(-80,81,20))
+m.drawmeridians(np.arange(-90,90,20))
+m.drawmapboundary()
+plt.title('EUMETSAT geostationary projection grid 2')
 
 plt.show()
