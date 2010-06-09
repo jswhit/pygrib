60a1b32e2997f4ac30a2e9a8a639a60f121296e1
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-06-09 18:11:25 +0000

    more flexible specification of lib and include dirs (*_LIBDIR, *_INCDIR env vars)
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@241 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- setup.py
+++ setup.py
@@ -3,41 +3,63 @@ import numpy
 import os
 
 grib_api_dir = os.environ.get('GRIBAPI_DIR')
+grib_api_libdir = os.environ.get('GRIBAPI_LIBDIR')
+grib_api_incdir = os.environ.get('GRIBAPI_INCDIR')
 jasper_dir = os.environ.get('JASPER_DIR')
+jasper_libdir = os.environ.get('JASPER_LIBDIR')
+jasper_incdir = os.environ.get('JASPER_INCDIR')
 png_dir = os.environ.get('PNG_DIR')
+png_libdir = os.environ.get('PNG_LIBDIR')
+png_incdir = os.environ.get('PNG_INCDIR')
 zlib_dir = os.environ.get('ZLIB_DIR')
+zlib_libdir = os.environ.get('ZLIB_LIBDIR')
+zlib_incdir = os.environ.get('ZLIB_INCDIR')
 openjpeg_dir = os.environ.get('OPENJPEG_DIR')
+openjpeg_libdir = os.environ.get('OPENJPEG_LIBDIR')
+openjpeg_incdir = os.environ.get('OPENJPEG_INCDIR')
 
 libdirs=[]
 incdirs=[numpy.get_include()]
 libraries=['grib_api']
 
-if grib_api_dir: 
-    incdirs.append(os.path.join(grib_api_dir,'include'))
-    libdirs.append(os.path.join(grib_api_dir,'lib64'))
+if grib_api_libdir is None and grib_api_dir is not None: 
     libdirs.append(os.path.join(grib_api_dir,'lib'))
-if openjpeg_dir: 
-    incdirs.append(os.path.join(openjpeg_dir,'include/openjpeg'))
-    incdirs.append(os.path.join(openjpeg_dir,'include'))
-    libdirs.append(os.path.join(openjpeg_dir,'lib64'))
-    libdirs.append(os.path.join(openjpeg_dir,'lib'))
-    libraries.append("openjpeg")
-if jasper_dir: 
-    incdirs.append(os.path.join(jasper_dir,'include/jasper'))
-    incdirs.append(os.path.join(jasper_dir,'include'))
-    libdirs.append(os.path.join(jasper_dir,'lib64'))
-    libdirs.append(os.path.join(jasper_dir,'lib'))
+    libdirs.append(os.path.join(grib_api_dir,'lib64'))
+if grib_api_incdir is None and grib_api_dir is not None: 
+    incdirs.append(os.path.join(grib_api_dir,'include'))
+
+if jasper_dir is not None or jasper_libdir is not None:
     libraries.append("jasper")
-if png_dir: 
-    incdirs.append(os.path.join(png_dir,'include'))
-    libdirs.append(os.path.join(png_dir,'lib64'))
-    libdirs.append(os.path.join(png_dir,'lib'))
+if jasper_libdir is None and jasper_dir is not None: 
+    libdirs.append(os.path.join(jasper_dir,'lib'))
+    libdirs.append(os.path.join(jasper_dir,'lib64'))
+if jasper_incdir is None and jasper_dir is not None: 
+    incdirs.append(os.path.join(jasper_dir,'include'))
+    incdirs.append(os.path.join(jasper_dir,'include/jasper'))
+
+if openjpeg_dir is not None or openjpeg_libdir is not None:
+    libraries.append("openjpeg")
+if openjpeg_libdir is None and openjpeg_dir is not None: 
+    libdirs.append(os.path.join(openjpeg_dir,'lib'))
+    libdirs.append(os.path.join(openjpeg_dir,'lib64'))
+if openjpeg_incdir is None and openjpeg_dir is not None: 
+    incdirs.append(os.path.join(openjpeg_dir,'include'))
+
+if png_dir is not None or png_libdir is not None:
     libraries.append("png")
-if zlib_dir: 
-    incdirs.append(os.path.join(zlib_dir,'include'))
-    libdirs.append(os.path.join(zlib_dir,'lib64'))
+if png_libdir is None and png_dir is not None: 
+    libdirs.append(os.path.join(png_dir,'lib'))
+    libdirs.append(os.path.join(png_dir,'lib64'))
+if png_incdir is None and png_dir is not None: 
+    incdirs.append(os.path.join(png_dir,'include'))
+
+if zlib_dir is not None or zlib_libdir is not None:
+    libraries.append("zlib")
+if zlib_libdir is None and zlib_dir is not None: 
     libdirs.append(os.path.join(zlib_dir,'lib'))
-    libraries.append("z")
+    libdirs.append(os.path.join(zlib_dir,'lib64'))
+if zlib_incdir is None and zlib_dir is not None: 
+    incdirs.append(os.path.join(zlib_dir,'include'))
 
 setup(name = "pygrib",
       version = "1.7",
