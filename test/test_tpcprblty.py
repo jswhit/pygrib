70aa5ca455faa907c2b7e32fd266543af89dec62
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2010-07-22 16:04:19 +0000

    add tropical cyclone prob example (tpc grib file)
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@276 68be0e0a-0922-11df-8158-c7cbc0dd6240

---
+++ test/test_tpcprblty.py
@@ -0,0 +1,24 @@
+import pygrib
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap,cm
+import numpy as np
+grbs = pygrib.open('../sampledata/tpcprblty.grib2')
+grb=grbs.select(name='Wind speed',scaledValueOfUpperLimit=17491,stepRange='0-120')[0]
+lats, lons = grb.latlons()
+data = grb.values
+map =\
+Basemap(projection='lcc',lon_0=-90,lat_0=30,width=4000.e3,height=2000.e3,resolution='l')
+map.bluemarble(scale=0.5)
+map.drawcoastlines(color='yellow')
+x,y = map(lons,lats)
+print data.min(), data.max()
+levels = np.arange(10,101,10)
+map.contourf(x,y,data,levels,cmap=cm.GMT_haxby_r)
+upperlim = float(grb.scaledValueOfUpperLimit)/np.power(10,grb.scaleFactorOfUpperLimit)
+plt.title('TC Wind Speed Prob > %5.2f m/s fcst hrs %s from %s' %\
+        (upperlim,grb.stepRange,repr(grb.dataDate)+repr(grb.dataTime)[0:2]),fontsize=12)
+map.drawparallels(np.arange(0,41,5),labels=[1,0,0,0])
+map.drawmeridians(np.arange(180,360,10),labels=[0,0,0,1])
+cb = plt.colorbar(orientation='horizontal')
+cb.ax.set_xlabel('percent')
+plt.show()
