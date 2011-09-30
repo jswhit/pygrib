f5905a2840c4f672e92ca62feafe769a2a918a32
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2011-09-30 01:58:17 +0000

    add ncepgrib2 docs
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@475 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- create_docs.sh
+++ create_docs.sh
@@ -1,2 +1,3 @@
 # svn propset svn:mime-type text/html docs/*html
 epydoc -v --no-frames --no-private --introspect-only -o docs pygrib
+epydoc -v --no-frames --no-private --introspect-only -o ncepgrib2_docs ncepgrib2
