e751e8dd590dd44db500b32146e4c12df4d6e320
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2011-10-08 20:32:55 +0000

    update sample gfs grib file
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@503 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- test.py
+++ test.py
@@ -157,26 +157,26 @@ def test():
     >>> grbs = pygrib.open('sampledata/gfs.grb')
     >>> sel_grbs = grbs.select(shortName='t',level=500)
     >>> for grb in sel_grbs: grb
-    39:Temperature:K (instant):regular_ll:isobaricInhPa:level 50000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
+    101:Temperature:K (instant):regular_ll:isobaricInhPa:level 50000 Pa:fcst time 72:from 201110080000
     >>> sel_grbs = grbs.select(shortName='t',level=(850,700,500))
     >>> for grb in sel_grbs: grb
-    32:Temperature:K (instant):regular_ll:isobaricInhPa:level 85000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
-    35:Temperature:K (instant):regular_ll:isobaricInhPa:level 70000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
-    39:Temperature:K (instant):regular_ll:isobaricInhPa:level 50000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
+    101:Temperature:K (instant):regular_ll:isobaricInhPa:level 50000 Pa:fcst time 72:from 201110080000
+    133:Temperature:K (instant):regular_ll:isobaricInhPa:level 70000 Pa:fcst time 72:from 201110080000
+    157:Temperature:K (instant):regular_ll:isobaricInhPa:level 85000 Pa:fcst time 72:from 201110080000
     >>> sel_grbs = grbs.select(shortName='t',level=lambda l: l < 500 and l >= 300)
     >>> for grb in sel_grbs: grb
-    40:Temperature:K (instant):regular_ll:isobaricInhPa:level 45000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
-    41:Temperature:K (instant):regular_ll:isobaricInhPa:level 40000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
-    42:Temperature:K (instant):regular_ll:isobaricInhPa:level 35000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
-    43:Temperature:K (instant):regular_ll:isobaricInhPa:level 30000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
+    69:Temperature:K (instant):regular_ll:isobaricInhPa:level 30000 Pa:fcst time 72:from 201110080000
+    77:Temperature:K (instant):regular_ll:isobaricInhPa:level 35000 Pa:fcst time 72:from 201110080000
+    85:Temperature:K (instant):regular_ll:isobaricInhPa:level 40000 Pa:fcst time 72:from 201110080000
+    93:Temperature:K (instant):regular_ll:isobaricInhPa:level 45000 Pa:fcst time 72:from 201110080000
     >>> grbstr = grb.tostring()
     >>> grb2 = pygrib.fromstring(grbstr)
     >>> grb2
-    1:Temperature:K (instant):regular_ll:isobaricInhPa:level 30000 Pa:fcst time 72:from 200412091200:lo res cntl fcst
+    1:Temperature:K (instant):regular_ll:isobaricInhPa:level 45000 Pa:fcst time 72:from 201110080000
     >>> grb2.analDate
-    datetime.datetime(2004, 12, 9, 12, 0)
+    datetime.datetime(2011, 10, 8, 0, 0)
     >>> grb2.validDate
-    datetime.datetime(2004, 12, 12, 12, 0)
+    datetime.datetime(2011, 10, 11, 0, 0)
     >>> grbs.close()
     >>> grbs = pygrib.open('sampledata/gfs.t12z.pgrbf120.2p5deg.grib2')
     >>> # see if multi-part grib messages are counted properly
