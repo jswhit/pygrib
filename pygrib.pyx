fe379d29d1c3a0c0dcb1df6333c7efc6e660f602
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2012-04-29 02:26:11 +0000

    another workaround for bugs in longitude calculations
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@600 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- pygrib.pyx
+++ pygrib.pyx
@@ -1452,9 +1452,11 @@ cdef class gribmessage(object):
             else:
                 lats = self['distinctLatitudes']
                 lons = self['distinctLongitudes']
-            # don't trust distinctLongitudes at all (bug in grib_api 1.9.16
-            # when longitudeOfLastGridPointInDegrees < 0).
-            lons = np.linspace(lon1,lon2,nx)
+            # don't trust distinctLongitudes at all 
+            # when longitudeOfLastGridPointInDegrees < 0
+            # (bug in grib_api 1.9.16)
+            if lon2 < 0:
+                lons = np.linspace(lon1,lon2,nx)
             lons,lats = np.meshgrid(lons,lats) 
         elif self['gridType'] == 'reduced_gg': # reduced global gaussian grid
             lats = self['distinctLatitudes']
