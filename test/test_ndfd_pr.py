ee49fc4690e0bbd96918e5470c097c7995f424e0
whitaker.jeffrey <whitaker.jeffrey@68be0e0a-0922-11df-8158-c7cbc0dd6240>
2013-11-22 19:26:25 +0000

    use grbs.select
    git-svn-id: https://pygrib.googlecode.com/svn/trunk@690 68be0e0a-0922-11df-8158-c7cbc0dd6240

--- test/test_ndfd_pr.py
+++ test/test_ndfd_pr.py
@@ -3,9 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 grbs = pygrib.open('../sampledata/dspr.temp.bin')
-for grb in grbs:
-    if grb['forecastTime'] == 12:
-        break
+grb = grbs.select(forecastTime=26)[0]
 data = grb['values']
 lats, lons = grb.latlons()
 llcrnrlon = lons[0,0]
