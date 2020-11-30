fd7e0415f77adb4137d42b14b64a72c0f5eb67f0
Jeff Whitaker <jswhit@fastmail.fm>
2020-11-30 16:11:16 -0700

    update

--- docs/conf.py
+++ docs/conf.py
@@ -81,6 +81,9 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'nature'
+html_css_files = [
+            'css/nature2.css',
+            ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
