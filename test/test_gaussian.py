428230b990e8ef4ba37cae593cb2a145413433ca
jswhit <whitaker.jeffrey@gmail.com>
2020-11-30 20:40:25 -0700

    migrate to cartopy

--- test/test_gaussian.py
+++ test/test_gaussian.py
@@ -1,15 +1,21 @@
 import pygrib
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+from cartopy.util import add_cyclic_point
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 grbs = pygrib.open('../sampledata/flux.grb')
 grb = grbs.message(2)
 lats, lons = grb.latlons()
-data = grb['values']
-m = Basemap(lon_0=180)
-#m.scatter(lons.flat,lats.flat,1,marker='o',color='k',zorder=10)
-x,y = m(lons,lats)
-m.drawcoastlines()
-m.contourf(x,y,data,15)
-#m.fillcontinents()
+lons1 = lons[0,:]; lats1 = lats[:,0]
+data = grb.values
+# add cyclic (wrap-around) point to global grid
+data,lons1 = add_cyclic_point(data, coord=lons1)
+lons,lats = np.meshgrid(lons1,lats1)
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+ax.coastlines()
+ax.contourf(lons,lats,data,15)
+# plot location of every 4th grid point
+plt.scatter(lons[::4,::4].ravel(),lats[::4,::4].ravel(),1,marker='o',color='k',zorder=10)
 plt.title('Global Gaussian Grid')
 plt.show()
