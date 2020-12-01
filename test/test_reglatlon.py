05ba4754f2a64410f50a6d5e22603b116bf7c395
jswhit <whitaker.jeffrey@gmail.com>
2020-12-01 15:17:44 -0700

    basemap -> cartopy

--- test/test_reglatlon.py
+++ test/test_reglatlon.py
@@ -1,6 +1,8 @@
 import pygrib
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+from cartopy.util import add_cyclic_point
+import cartopy.crs as ccrs
 
 grbs = pygrib.open('../sampledata/gfs.grb')
 grb = grbs.select(name='Orography')[0]
@@ -8,23 +10,31 @@ grb2 = grbs.select(name='Volumetric soil moisture content')[0]
 
 data = grb.values; lats,lons = grb.latlons()
 data2 = grb2.values
+lons1d = lons[0,:]; lats1d = lats[:,0]
+# add cyclic (wrap-around) point to global grid
+data,lons1 = add_cyclic_point(data, coord=lons1d)
+data2,lons1 = add_cyclic_point(data2, coord=lons1d)
+lons,lats = np.meshgrid(lons1,lats1d)
 
-m = Basemap(llcrnrlon=lons.min(),llcrnrlat=lats.min(),urcrnrlon=lons.max(),urcrnrlat=lats.max(),projection='cyl')
-m.drawcoastlines()
-m.contourf(lons,lats,data,15,cmap=plt.cm.hot_r)
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+ax.coastlines()
+ax.contourf(lons,lats,data,15,cmap=plt.cm.hot_r)
 plt.title('%s Global Lat/Lon Grid' % grb.name)
+
 fig = plt.figure()
-m.drawcoastlines()
-m.contourf(lons,lats,data2,15)
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+ax.coastlines()
+ax.contourf(lons,lats,data2,15)
 plt.title('%s Global Lat/Lon Grid' % grb2.name)
 
 lat1=15; lat2=65; lon1=220; lon2=320
 datsubset,latsubset,lonsubset=grb.data(lat1=lat1,lat2=lat2,lon1=lon1,lon2=lon2)
 
 fig = plt.figure()
-m = Basemap(llcrnrlon=lon1,llcrnrlat=lat1,urcrnrlon=lon2,urcrnrlat=lat2,projection='cyl',resolution='l')
-m.drawcoastlines()
-m.contourf(lonsubset,latsubset,datsubset,15,cmap=plt.cm.hot_r)
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+ax.set_extent([lon1,lon2,lat1,lat2],crs=ccrs.PlateCarree())
+ax.coastlines()
+ax.contourf(lonsubset,latsubset,datsubset,15,cmap=plt.cm.hot_r)
 plt.title('%s Regional Lat/Lon Grid' % grb.name)
 
 plt.show()
