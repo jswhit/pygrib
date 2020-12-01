36eabd980205db9dae4724566200b99dbe03ea79
jswhit <whitaker.jeffrey@gmail.com>
2020-12-01 13:04:17 -0700

    basemap -> cartopy

--- test/animate.py
+++ test/animate.py
@@ -1,39 +1,44 @@
 import pygrib, time
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.basemap import Basemap
+from cartopy.util import add_cyclic_point
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
 import matplotlib.animation as animation
 
 # animation example.
 
 grbs = pygrib.open('../sampledata/safrica.grib2')
 # grab all "brightness temp" grib messages.
-btemps = [grb for grb in grbs if grb['name']=='Brightness temperature']
+btemps = [grb for grb in grbs if grb.name=='Brightness temperature']
 grb = btemps[0]
 lats, lons = grb.latlons()
 projd = grb.projparams
+print(projd)
 grbs.close()
 
 # create a map projection for the domain, plot 1st image on it.
-m =\
-Basemap(projection=projd['proj'],lat_ts=projd['lat_ts'],lon_0=projd['lon_0'],\
-        lat_0=projd['lat_0'],rsphere=(projd['a'],projd['b']),\
-        llcrnrlat=lats[0,0],urcrnrlat=lats[-1,-1],\
-        llcrnrlon=lons[0,0],urcrnrlon=lons[-1,-1],resolution='i')
 fig = plt.figure(figsize=(8,7))
-m.drawcoastlines()
-m.drawcountries()
+globe = ccrs.Globe(ellipse='sphere', semimajor_axis=projd['a'], semiminor_axis=projd['b'])
+pj = ccrs.Stereographic(globe=globe,central_longitude=projd['lon_0'],
+     central_latitude=projd['lat_0'],
+     true_scale_latitude=projd['lat_ts']) 
+ax = fig.add_subplot(1,1,1,projection=pj)
+coords = pj.transform_points(                                                                
+    ccrs.PlateCarree(), np.asarray([lons[0,0],lons[-1,-1]]), np.asarray([lats[0,0],lats[-1,-1]]))
+ax.set_extent([coords[0, 0], coords[1, 0], coords[0, 1], coords[1, 1]], crs=pj)
+ax.coastlines()
+ax.add_feature(cfeature.BORDERS, linestyle='-');
 grb = btemps[0]
-im = m.imshow(grb['values'],interpolation='nearest',vmin=230,vmax=310)
-plt.colorbar(orientation='horizontal')
-m.drawparallels(np.arange(-80,10,10),labels=[1,0,0,0])
-m.drawmeridians(np.arange(-80,81,20),labels=[0,0,0,1])
+im = plt.imshow(np.empty(lons.shape),vmin=230,vmax=310,transform=ccrs.PlateCarree(),origin='lower')
+im.set_data(grb.values)
+plt.colorbar(im,orientation='horizontal')
 txt = plt.title(grb,fontsize=8)
 
 def updatefig(nt):
     global im,txt,btemps,cnt,delay
     grb = btemps[nt]
-    im.set_data(grb['values'])
+    im.set_data(grb.values)
     txt.set_text(repr(grb))
 
 ani = animation.FuncAnimation(fig, updatefig, frames=len(btemps))
