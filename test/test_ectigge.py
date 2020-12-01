9518764eac66e07d782532907ed38e2cacdd0029
jswhit <whitaker.jeffrey@gmail.com>
2020-12-01 09:47:24 -0700

    basemap --> cartopy

--- test/test_ectigge.py
+++ test/test_ectigge.py
@@ -2,24 +2,26 @@ import pygrib
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import ma
-from mpl_toolkits.basemap import Basemap
+from cartopy.util import add_cyclic_point
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import matplotlib.ticker as mticker
 
 
 grbs = pygrib.open('../sampledata/ecmwf_tigge.grb')
 grb = grbs.select(parameterName='Soil moisture')[0]
 fld = grb.values; lats,lons = grb.latlons()
+lons1 = lons[0,:]; lats1 = lats[:,0]
+# add cyclic (wrap-around) point to global grid
+fld,lons1 = add_cyclic_point(fld, coord=lons1)
+lons,lats = np.meshgrid(lons1,lats1)
 
-m = Basemap(lon_0=180)
-CS = m.contourf(lons,lats,fld,15,cmap=plt.cm.jet)
-plt.colorbar(shrink=0.6)
-m.drawcoastlines()
-# draw parallels
-delat = 30.
-circles = np.arange(-90.,90.+delat,delat)
-m.drawparallels(circles,labels=[1,0,0,0])
-# draw meridians
-delon = 60.
-meridians = np.arange(0,360,delon)
-m.drawmeridians(meridians,labels=[0,0,0,1])
-plt.title(grb['parameterName']+' on ECMWF Reduced Gaussian Grid')
+ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=0))
+cs = ax.contourf(lons,lats,fld,15,cmap=plt.cm.jet)
+plt.colorbar(cs, shrink=0.6)
+ax.coastlines()
+gl = ax.gridlines(draw_labels=True)
+gl.ylabels_top = False; gl.xlabels_top = False
+gl.ylabels_right = False; gl.xlabels_right = False
+plt.title(grb.parameterName+' on ECMWF Reduced Gaussian Grid')
 plt.show()
