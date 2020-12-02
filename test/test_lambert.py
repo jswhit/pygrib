3af474892677fa65bf57539a6ffe0acbbc6c140d
jswhit <whitaker.jeffrey@gmail.com>
2020-12-02 10:14:30 -0700

    basemap --> cartopy

--- test/test_lambert.py
+++ test/test_lambert.py
@@ -1,26 +1,24 @@
 import pygrib
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import cartopy.crs as ccrs
+
 grbs = pygrib.open('../sampledata/eta.grb')
 grb = grbs.select(parameterName='Pressure',typeOfLevel='surface')[0]
 data = grb.values
 lats, lons = grb.latlons()
-llcrnrlon = lons[0,0]
-llcrnrlat = lats[0,0]
-urcrnrlon = lons[-1,-1]
-urcrnrlat = lats[-1,-1]
-rsphere = (grb.projparams['a'], grb.projparams['b'])
-lon_0 = grb.projparams['lon_0']
-lat_1 = grb.projparams['lat_1']
-lat_2 = grb.projparams['lat_2']
-projection = grb.projparams['proj']
-m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
-            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,rsphere=rsphere,lon_0=lon_0,
-            lat_1=lat_1,lat_2=lat_2,resolution='l',projection=projection)
-x,y = m(lons, lats)
-m.scatter(x.flat,y.flat,3,marker='o',color='k',zorder=10)
-m.drawcoastlines()
-x,y = m(lons,lats)
-m.contourf(x,y,data,15)
+
+globe = ccrs.Globe(ellipse='sphere', semimajor_axis=grb.projparams['a'], semiminor_axis=grb.projparams['b'])
+pj = ccrs.LambertConformal(globe=globe,central_longitude=grb.projparams['lon_0'],
+     central_latitude=grb.projparams['lat_0'],
+     standard_parallels =(grb.projparams['lat_1'],grb.projparams['lat_2']))
+ax = plt.axes(projection=pj)
+coords = pj.transform_points(                                                                
+         ccrs.PlateCarree(), np.asarray([lons[0,0],lons[-1,-1]]), np.asarray([lats[0,0],lats[-1,-1]]))
+ax.set_extent([coords[0, 0], coords[1, 0], coords[0, 1], coords[1, 1]], crs=pj)
+ax.scatter(lons.flat,lats.flat,3,marker='o',color='k',zorder=10,transform=ccrs.PlateCarree())
+ax.coastlines()
+coords = pj.transform_points(ccrs.PlateCarree(), lons, lats)
+cs = ax.contourf(coords[:,:,0],coords[:,:,1],data,15)
 plt.title('Lambert Conformal Model Grid')
 plt.show()
