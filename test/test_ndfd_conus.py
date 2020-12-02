0088100d47f50670342644cf06c0544383d79f07
jswhit <whitaker.jeffrey@gmail.com>
2020-12-02 10:36:45 -0700

    basemap --> cartopy

--- test/test_ndfd_conus.py
+++ test/test_ndfd_conus.py
@@ -1,33 +1,30 @@
 import pygrib, sys
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+
 grbs = pygrib.open('../sampledata/ds.maxt.bin')
-g = grbs.message(1)
-lats, lons = g.latlons()
-data = g.values
-llcrnrlon = lons[0,0]
-llcrnrlat = lats[0,0]
-urcrnrlon = lons[-1,-1]
-urcrnrlat = lats[-1,-1]
-rsphere = (g.projparams['a'],g.projparams['b'])
-lat_1 = g.projparams['lat_1']
-lat_2 = g.projparams['lat_2']
-lon_0 = g.projparams['lon_0']
-projection = g.projparams['proj']
-fig=plt.figure()
-sys.stdout.write(repr(g.projparams)+'\n')
-ax = fig.add_axes([0.1,0.1,0.75,0.75])
-m = Basemap(llcrnrlon=llcrnrlon,llcrnrlat=llcrnrlat,
-            urcrnrlon=urcrnrlon,urcrnrlat=urcrnrlat,rsphere=rsphere,lon_0=lon_0,
-            lat_1=lat_1,lat_2=lat_2,resolution='l',projection=projection,area_thresh=10000)
-x,y = m(lons, lats)
-cs = m.contourf(x,y,data,20,cmap=plt.cm.jet)
-m.drawcoastlines()
-m.drawstates()
-m.drawcountries()
+grb = grbs.message(1)
+lats, lons = grb.latlons()
+data = grb.values
+
+globe = ccrs.Globe(ellipse='sphere', semimajor_axis=grb.projparams['a'], semiminor_axis=grb.projparams['b'])
+pj = ccrs.LambertConformal(globe=globe,central_longitude=grb.projparams['lon_0'],
+     central_latitude=grb.projparams['lat_0'],
+     standard_parallels =(grb.projparams['lat_1'],grb.projparams['lat_2']))
+ax = plt.axes([0.1,0.1,0.75,0.75],projection=pj)
+coords = pj.transform_points(                                                                
+         ccrs.PlateCarree(), np.asarray([lons[0,0],lons[-1,-1]]), np.asarray([lats[0,0],lats[-1,-1]]))
+ax.set_extent([coords[0, 0], coords[1, 0], coords[0, 1], coords[1, 1]], crs=pj)
+ax.coastlines()
+ax.add_feature(cfeature.BORDERS, linestyle='-');
+ax.add_feature(cfeature.STATES, linestyle='-');
+coords = pj.transform_points(ccrs.PlateCarree(), lons, lats)
+cs = ax.contourf(coords[:,:,0],coords[:,:,1],data,20,cmap=plt.cm.jet)
 # new axis for colorbar.
 cax = plt.axes([0.875, 0.15, 0.03, 0.65])
 plt.colorbar(cs, cax, format='%g') # draw colorbar
 plt.axes(ax)  # make the original axes current again
-plt.title('NDFD Temp CONUS %d-h forecast'% g['forecastTime'],fontsize=12)
+plt.title('NDFD Temp CONUS %d-h forecast'% grb.forecastTime,fontsize=12)
 plt.show()
