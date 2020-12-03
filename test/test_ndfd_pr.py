1fc411750c5d8a0ef646d5ea23dec611ef9707be
jswhit <whitaker.jeffrey@gmail.com>
2020-12-02 20:58:32 -0700

    update

--- test/test_ndfd_pr.py
+++ test/test_ndfd_pr.py
@@ -17,7 +17,8 @@ ax = plt.axes([0.1,0.1,0.75,0.75],projection=pj)
 coords = pj.transform_points(                                                                
          ccrs.PlateCarree(), np.asarray([lons[0,0],lons[-1,-1]]), np.asarray([lats[0,0],lats[-1,-1]]))
 ax.set_extent([coords[0, 0], coords[1, 0], coords[0, 1], coords[1, 1]], crs=pj)
-ax.coastlines(resolution='50m')
+#ax.coastlines(resolution='50m')
+ax.coastlines()
 ax.add_feature(cfeature.BORDERS, linestyle='-');
 ax.add_feature(cfeature.STATES, linestyle='-');
 coords = pj.transform_points(ccrs.PlateCarree(), lons, lats)
@@ -31,6 +32,7 @@ gl.ylabels_top = False; gl.xlabels_top = False
 gl.ylabels_right = False; gl.xlabels_right = False
 plt.title('NDFD Temp Puerto Rico %d-h fcst from %d' %\
         (grb.forecastTime,grb.dataDate),fontsize=12)
+plt.savefig('ndfd_pr_baseline.png')
 # raise exception if generated image doesn't match baseline 
 plt.savefig('ndfd_pr.png')
 assert( compare_images('ndfd_pr_baseline.png','ndfd_pr.png',10) is None )
