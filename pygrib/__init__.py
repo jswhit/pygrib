a8267261c57a81a6632f030d5ee6fb68447bd37e
Jeff Whitaker <jswhit@fastmail.fm>
2020-12-20 15:58:40 -0700

    set 'ECCODES_DEFINITION_PATH'

---
+++ pygrib/__init__.py
@@ -0,0 +1,5 @@
+# init for pygrib package
+from ._datdir import eccodes_datadir
+from ._pygrib import *
+# Need explicit imports for names beginning with underscores
+from ._pygrib import __doc__, __version__
---
+++ pygrib/_datadir.py
@@ -0,0 +1,8 @@
+import os
+
+if 'ECCODES_DEFINITION_PATH' in os.environ:
+    eccodes_datadir = os.environ['ECCODES_DEFINITION_PATH']
+else:
+    eccodes_datadir = os.sep.join([os.path.dirname(__file__),
+        'eccodes/definitions'])
+    os.environ['ECCODES_DEFINITION_PATH'] = eccodes_datadir
