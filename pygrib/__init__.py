e0407fd2d4c2f40c561524d3783f6c0f5cb205e3
Jeff Whitaker <jswhit@fastmail.fm>
2020-12-27 10:00:11 -0700

    print more informative error message when importing from install dir

--- pygrib/__init__.py
+++ pygrib/__init__.py
@@ -1,4 +1,16 @@
-# init for pygrib package
-from ._pygrib import *
+# When importing from the root of the unpacked tarball or git checkout,
+# Python sees the "pygrib" source directory and tries to load it, which fails.
+# (unless the package was build in-place using "python setup.py build_ext
+# --inplace" so that _pygrib.so exists in the pygrib source dir).
+try:
+    # init for pygrib package
+    from ._pygrib import *
+except ImportError:
+    import os.path as _op
+    if _op.exists(_op.join(_op.dirname(__file__), '..', 'setup.py')):
+        msg="You cannot import pygrib from inside the install directory.\nChange to another directory first."
+        raise ImportError(msg)
+    else:
+        raise
 # Need explicit imports for names beginning with underscores
 from ._pygrib import __doc__, __version__
