c895d4bef650699053fce5d03e5edff9f62d17fc
Jason Leaver <jason.leaver.developer@gmail.com>
2022-05-07 12:33:57 -0500

    format

--- src/pygrib/__init__.py
+++ src/pygrib/__init__.py
@@ -1,7 +1,6 @@
 """High-level python interface to ECCODES library for GRIB file IO."""
 # init for pygrib package
 from ._pygrib import *
-# from ._pygrib import open
 # Need explicit imports for names beginning with underscores
 from ._pygrib import __doc__, __version__
 from ._ctx import read as read
