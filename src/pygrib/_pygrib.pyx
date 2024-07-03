219651b498cf5280d3b12ed243e22b391a4803d6
jswhit <whitaker.jeffrey@gmail.com>
2024-07-03 09:55:57 -0600

    update

--- src/pygrib/_pygrib.pyx
+++ src/pygrib/_pygrib.pyx
@@ -1199,7 +1199,7 @@ cdef class gribmessage(object):
                     raise RuntimeError(_get_error_message(err))
                 return longval
             else: # array
-                datarr = np.zeros(size, np.int_))
+                datarr = np.zeros(size, np.int_)
                 err = grib_get_long_array(self._gh, name, <long *>datarr.data, &size)
                 if err:
                     raise RuntimeError(_get_error_message(err))
